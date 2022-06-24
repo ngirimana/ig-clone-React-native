72b7d080b8d3ece465e2c1bf8ce39eae0c9a86aa
Schadrack <chadrackngirimana@gmail.com>
2022-06-24 15:04:17 +0200

    stories component

---
+++ data/users.js
@@ -0,0 +1,34 @@
+export const USERS = [
+  {
+    user: "user1",
+    image: "https://i.imgur.com/dxjd7FB.jpg",
+  },
+  {
+    user: "user3",
+    image: "https://statinfer.com/wp-content/uploads/dummy-user.png",
+  },
+  {
+    user: "user1",
+    image: "https://i.imgur.com/dxjd7FB.jpg",
+  },
+  {
+    user: "user5",
+    image: "https://statinfer.com/wp-content/uploads/dummy-user.png",
+  },
+  {
+    user: "user1",
+    image: "https://i.imgur.com/dxjd7FB.jpg",
+  },
+  {
+    user: "user6",
+    image: "https://statinfer.com/wp-content/uploads/dummy-user.png",
+  },
+  {
+    user: "user1",
+    image: "https://i.imgur.com/dxjd7FB.jpg",
+  },
+  {
+    user: "user7",
+    image: "https://statinfer.com/wp-content/uploads/dummy-user.png",
+  },
+];
